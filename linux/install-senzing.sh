e5104b3fc28fc91bc7278242d15691bcfe0a05ff
Sam <109683132+kernelsam@users.noreply.github.com>
2024-05-06 08:48:47 -0700

    add openssl3 support

--- linux/install-senzing.sh
+++ linux/install-senzing.sh
@@ -11,8 +11,12 @@ set -e
 configure-vars() {
 
   # senzing apt repository packages
-  PROD_REPO=https://senzing-production-apt.s3.amazonaws.com/senzingrepo_1.0.1-1_all.deb
-  STAGING_REPO=https://senzing-staging-apt.s3.amazonaws.com/senzingstagingrepo_1.0.1-1_all.deb
+  # v3 and lower
+  PROD_REPO_V3_AND_LOWER=https://senzing-production-apt.s3.amazonaws.com/senzingrepo_1.0.1-1_all.deb
+  STAGING_REPO_V3_AND_LOWER=https://senzing-staging-apt.s3.amazonaws.com/senzingstagingrepo_1.0.1-1_all.deb
+  # v4 and above
+  PROD_REPO_V4_AND_ABOVE=https://senzing-production-apt.s3.amazonaws.com/senzingrepo_2.0.0-1_all.deb
+  STAGING_REPO_V4_AND_ABOVE=https://senzing-staging-apt.s3.amazonaws.com/senzingstagingrepo_2.0.0-1_all.deb
 
   # semantic versions
   REGEX_SEM_VER="^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)$"
@@ -22,26 +26,30 @@ configure-vars() {
   if [[ $SENZING_INSTALL_VERSION =~ "production" ]]; then
 
     echo "[INFO] install senzingapi-runtime from production"
-    INSTALL_REPO="$PROD_REPO"
+    get-generic-major-version
+    is-major-version-greater-than-3 && INSTALL_REPO="$PROD_REPO_V4_AND_ABOVE" || INSTALL_REPO="$PROD_REPO_V3_AND_LOWER"
     SENZING_PACKAGE="senzingapi-runtime"
     restrict-major-version
 
   elif [[ $SENZING_INSTALL_VERSION =~ "staging" ]]; then
 
     echo "[INFO] install senzingapi-runtime from staging"
-    INSTALL_REPO="$STAGING_REPO"
+    get-generic-major-version
+    is-major-version-greater-than-3 && INSTALL_REPO="$STAGING_REPO_V4_AND_ABOVE" || INSTALL_REPO="$STAGING_REPO_V3_AND_LOWER"
     SENZING_PACKAGE="senzingapi-runtime"
     restrict-major-version
 
   elif [[ $SENZING_INSTALL_VERSION =~ $REGEX_SEM_VER ]]; then
   
     echo "[INFO] install senzingapi-runtime semantic version"
+    is-major-version-greater-than-3 && INSTALL_REPO="$PROD_REPO_V4_AND_ABOVE" || INSTALL_REPO="$PROD_REPO_V3_AND_LOWER"
     INSTALL_REPO="$PROD_REPO"
     SENZING_PACKAGE="senzingapi-runtime=$SENZING_INSTALL_VERSION*"
 
   elif [[ $SENZING_INSTALL_VERSION =~ $REGEX_SEM_VER_BUILD_NUM ]]; then
 
     echo "[INFO] install senzingapi-runtime semantic version with build number"
+    is-major-version-greater-than-3 && INSTALL_REPO="$PROD_REPO_V4_AND_ABOVE" || INSTALL_REPO="$PROD_REPO_V3_AND_LOWER"
     INSTALL_REPO="$PROD_REPO"
     SENZING_PACKAGE="senzingapi-runtime=$SENZING_INSTALL_VERSION"
 
@@ -53,7 +61,54 @@ configure-vars() {
 }
 
 ############################################
-# install-senzing-repository
+# is-major-version-greater-than-3
+# GLOBALS:
+#   MAJOR_VERSION
+#     set prior to this call via either
+#     get-generic-major-version or
+#     get-semantic-major-version
+############################################
+is-major-version-greater-than-3() {
+
+  if [[ $MAJOR_VERSION -gt 3 ]]; then
+    return true
+  else
+    return false
+  fi
+
+}
+
+############################################
+# get-generic-major-version
+# GLOBALS:
+#   SENZING_INSTALL_VERSION
+#     one of: production-v<X>, staging-v<X>
+#     semver does not apply here
+############################################
+get-generic-major-version(){
+
+  MAJOR_VERSION=$(echo "$SENZING_INSTALL_VERSION" | grep -Eo '[0-9]+$')
+  export MAJOR_VERSION
+
+}
+
+############################################
+# get-semantic-major-version
+# GLOBALS:
+#   SENZING_INSTALL_VERSION
+#     one of: X.Y.Z, X.Y.Z-ABCDE
+#     production-v<X> and staging-v<X> 
+#     does not apply here
+############################################
+get-semantic-major-version(){
+
+  MAJOR_VERSION=$(echo ${SENZING_INSTALL_VERSION%%.*})
+  export MAJOR_VERSION
+
+}
+
+############################################
+# restrict-major-version
 # GLOBALS:
 #   SENZING_INSTALL_VERSION
 #     one of: production-v<X>, staging-v<X>
@@ -61,7 +116,7 @@ configure-vars() {
 ############################################
 restrict-major-version() {
 
-  MAJOR_VERSION=$(echo "$SENZING_INSTALL_VERSION" | grep -Eo '[0-9]+$')
+  get-generic-major-version
   senzingapi_runtime_preferences_file="/etc/apt/preferences.d/senzingapi-runtime"
   echo "[INFO] restrict senzingapi-runtime major version to: $MAJOR_VERSION"
 
